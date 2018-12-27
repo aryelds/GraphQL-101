eaa48895c5d89a320e85576c538ef5f5e457a2cc
Aryel Santos <aryel.ds@gmail.com>
2018-12-27 00:12:30 -0200

    Add object array and fake data

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 import { GraphQLServer } from 'graphql-yoga'
+import faker from 'faker';
+
+const users = [{
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    age: faker.random.number({min : 1, max: 100})
+}, {
+    id: faker.random.uuid(),
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    age: faker.random.number({min : 1, max: 100})
+}];
+
+const posts = [{
+    id: faker.random.uuid(),
+    title: faker.lorem.text(),
+    body: faker.lorem.paragraph(),
+    published: faker.random.boolean()
+}, {
+    id: faker.random.uuid(),
+    title: faker.lorem.text(),
+    body: faker.lorem.paragraph(),
+    published: faker.random.boolean()
+}];
 
 const typeDefs = `
     type Query {
-      users: [User!]!  
+      users(query:String): [User!]!  
       me: User!
-      post: Post!
+      posts(query:String): [Post!]!
     }
-    
     type User {
         id: ID!
         name: String!
-        email: String!
         age: Int
     }
-    
     type Post {
         id: ID!
         title: String!
@@ -24,21 +46,33 @@ const typeDefs = `
 
 const resolvers = {
     Query: {
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
+            }
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase());
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase());
+                return isTitleMatch || isBodyMatch
+            })
+        },
         me() {
             return {
                id: 123098,
                name: 'maria',
-               email: 'maria@examplo.com',
+               email: 'maria@example.com',
            }
-       },
-        post() {
-            return {
-                id: '092',
-                title: 'GraphQL 101',
-                body: '',
-                published: false
-            }
-        }
+       }
     }
 };
 
@@ -48,5 +82,6 @@ const server = new GraphQLServer({
 });
 
 server.start(() => {
+    console.log(faker);
    console.log('The server is up!')
 });
